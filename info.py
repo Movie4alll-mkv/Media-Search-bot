7dc61ce8de3cdb69850e142b3b6932eff1f21a4b
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-05-20 12:45:37 +0500

    Update info.py

--- info.py
+++ info.py
@@ -1,39 +1,32 @@
-import re
-from os import environ
-
-id_pattern = re.compile(r'^.\d+$')
-
 # Bot information
-SESSION = environ.get('SESSION', 'Media_search')
-USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_HASH = environ['API_HASH','4bcc61d959a9f403b2f20149cbbe627a']
-BOT_TOKEN = environ['BOT_TOKEN','5442493323:AAGE585VqW2Rjn8p7fTamBdyiSsg9dktdgE']
-USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
-API_ID = int(environ['API_ID',6534707])
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 6534707
+API_HASH = '4bcc61d959a9f403b2f20149cbbe627a'
+BOT_TOKEN = '5541380668:AAGH9yGr_4leFAgG35raDvAi1Q82ynMJGG0'
+USERBOT_STRING_SESSION = ''
+
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS','1430593323'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS','animedualaudiozippercartoonist'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1430593323').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+ADMINS = [1430593323]
+CHANNELS = ['animedualaudiozippercartoonist']
+AUTH_USERS = []
+AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI','mongodb+srv://Uploader:Uploader@cluster0.ba0ppxa.mongodb.net/?retryWrites=true&w=majority']
-DATABASE_NAME = environ['DATABASE_NAME','maj']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://Uploader:Uploader@cluster0.ba0ppxa.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
-default_start_msg = """
+START_MSG = """
 **Hi, I'm Media Search bot**
 
-Here you can search files in inline mode. Just press following buttons and start searching.
+Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
 
-START_MSG = environ.get('START_MSG', default_start_msg)
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = environ.get('INVITE_MSG', 'Please join @.... to use this bot')
+INVITE_MSG = 'Please join @.... to use this bot'
